f57cb1cc74e6cf643c04667b0804016db5d32f69
hooneun <kimhoon0316@gmail.com>
2020-12-01 23:09:49 +0900

    model, dblayer, rest add

---
+++ backend/src/dblayer/dblayer.go
@@ -0,0 +1,18 @@
+package dblayer
+
+import (
+	"github.com/hooneun/go-music/backend/models"
+)
+
+// DBLayer interface
+type DBLayer interface {
+	GetAppProducts() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName(string, string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(int) (models.Product, error)
+	AddUser(models.Customer) (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserByID(int) error
+	GetCustomerOrdersByID(int) ([]models.Order, error)
+}
