42f6d7fb0df5757cd0c72afceece90b09daff8ec
hooneun <kimhoon0316@gmail.com>
2020-12-09 22:22:27 +0900

    add. orm ing

--- backend/src/dblayer/orm.go
+++ backend/src/dblayer/orm.go
@@ -1,6 +1,7 @@
 package dblayer
 
 import (
+	"github.com/hooneun/go-music/backend/src/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -16,4 +17,21 @@ func NewORM(dns string) (*DBORM, error) {
 	return &DBORM{
 		DB: db,
 	}, err
+}
+
+// GetAllProducts All products
+func (db *DBORM) GetAllProducts() (products []models.Product, err error) {
+	return products, db.Find(&products).Error
+}
+
+// GetPromos promotion products
+func (db *DBORM) GetPromos(product []models.Product, err error) {
+	return products, db.Where("promotion IS NOT NULL").Find(&products).Error
+}
+
+// GetCustomerByName !
+func (db *DBORM) GetCustomerByName(name string) (customer models.Customer, err error) {
+	return customer, db.Where(&models.Customer{
+		Name: name,
+	}).Find(&customer).Error
 }
