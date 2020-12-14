c4685760155bc833d8b227065c60351c6bb81e68
hooneun <kimhoon0316@gmail.com>
2020-12-14 23:25:28 +0900

    add. orm ing

--- backend/src/dblayer/orm.go
+++ backend/src/dblayer/orm.go
@@ -1,6 +1,8 @@
 package dblayer
 
 import (
+	"errors"
+
 	"github.com/hooneun/go-music/backend/src/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -52,4 +54,37 @@ func (db *DBORM) AddUser(customer models.Customer) (models.Customer, error) {
 	customer.LoggedIn = true
 
 	return customer, db.Create(&customer).Error
+}
+
+// SignInUser user sign in
+func (db *DBORM) SignInUser(email, password string) (customer models.Customer, err error) {
+	if !checkPassword(password) {
+		return customer, errors.New("Invalid password")
+	}
+
+	result := db.Table("customers").Where(&models.Customer{
+		Email: email,
+	})
+	err = result.Update("loggeding", 1).Error
+	if err != nil {
+		return customer, err
+	}
+
+	return customer, result.Find(&customer).Error
+}
+
+// SignOutUserByID ~
+func (db *DBORM) SignOutUserByID(id int) error {
+	customer := models.Customer{
+		Model: gorm.Model{
+			ID: uint(id),
+		},
+	}
+
+	return db.Table("customers").Where(&customer).Update("loggedin", 0).Error
+}
+
+// GetCustomerOrdersByID !
+func (db *DBORM) GetCustomerOrdersByID(id int) (orders []models.Orders, err error) {
+	return orders, db.Table("orders").Select("*").Joins("JOIN customers ON customers.id = customer_id").Joins("JOIN products ON products.id = product_id").Where("customer_id = ?", id).Scan(&orders).Error
 }
