53de61dc00543f2380b40aa3ed6f18b3185d6b28
hooneun <kimhoon0316@gmail.com>
2020-12-15 22:54:04 +0900

    add. hash password

--- backend/src/dblayer/orm.go
+++ backend/src/dblayer/orm.go
@@ -3,6 +3,8 @@ package dblayer
 import (
 	"errors"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/hooneun/go-music/backend/src/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -58,18 +60,22 @@ func (db *DBORM) AddUser(customer models.Customer) (models.Customer, error) {
 
 // SignInUser user sign in
 func (db *DBORM) SignInUser(email, password string) (customer models.Customer, err error) {
-	if !checkPassword(password) {
-		return customer, errors.New("Invalid password")
-	}
-
 	result := db.Table("customers").Where(&models.Customer{
 		Email: email,
 	})
-	err = result.Update("loggeding", 1).Error
+	err = result.First(&customer).Error
 	if err != nil {
 		return customer, err
 	}
+	if !checkPassword(customer.Password, password) {
+		return customer, errors.New("Invalid password")
+	}
 
+	customer.Password = ""
+	err = result.Update("loggedin", 1).Error
+	if err != nil {
+		return customer, err
+	}
 	return customer, result.Find(&customer).Error
 }
 
@@ -85,6 +91,25 @@ func (db *DBORM) SignOutUserByID(id int) error {
 }
 
 // GetCustomerOrdersByID !
-func (db *DBORM) GetCustomerOrdersByID(id int) (orders []models.Orders, err error) {
+func (db *DBORM) GetCustomerOrdersByID(id int) (orders []models.Order, err error) {
 	return orders, db.Table("orders").Select("*").Joins("JOIN customers ON customers.id = customer_id").Joins("JOIN products ON products.id = product_id").Where("customer_id = ?", id).Scan(&orders).Error
+}
+
+// hashPassword !
+func hashPassword(s *string) error {
+	if s == nil {
+		return errors.New("Reference provided for hashing password is nil")
+	}
+	sBytes := []byte(*s)
+	hashedBytes, err := bcrypt.GenerateFromPassword(sBytes, bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	*s = string(hashedBytes[:])
+	return nil
+}
+
+// checkPassword !
+func checkPassword(existingHash, incomingPassword string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(existingHash), []byte(incomingPassword)) == nil
 }
